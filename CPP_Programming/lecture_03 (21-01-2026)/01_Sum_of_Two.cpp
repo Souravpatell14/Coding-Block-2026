6e4e6e01f03a4d27c89ebf891461b0136bffc8da
Souravpatell14 <sourabhpatel1415@gmail.com>
2026-01-21 08:52:41 -0500

    update

---
+++ CPP_Programming/lecture_03 (21-01-2026)/01_Sum_of_Two.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+
+
+void sum_of_Two(int a, int b){
+    cout<<a+b<<endl;
+}
+
+int main(){
+    int a,b;
+    cin>>a>>b;
+
+    sum_of_Two(a,b);
+    return 0;
+}
