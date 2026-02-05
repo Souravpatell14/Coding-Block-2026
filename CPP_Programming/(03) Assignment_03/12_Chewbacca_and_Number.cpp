fa9ff297511c7384e6ec32eb175facadc49ac939
Souravpatell14 <sourabhpatel1415@gmail.com>
2026-02-05 10:27:18 -0500

    Added a program

---
+++ CPP_Programming/(03) Assignment_03/12_Chewbacca_and_Number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string x;
+    cin >> x;
+
+    for (int i = 0; i < x.length(); i++) {
+        int digit = x[i] - '0';
+        int inverted = 9 - digit;
+
+        if (i == 0 && inverted == 0) {
+            continue;
+        }
+        if (inverted < digit) {
+            x[i] = inverted + '0';
+        }
+    }
+    cout << x;
+    return 0;
+}
