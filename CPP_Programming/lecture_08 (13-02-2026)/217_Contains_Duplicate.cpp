007f331d8df37f2d59db745e0333ecc425da9792
Souravpatell14 <sourabhpatel1415@gmail.com>
2026-02-14 00:30:40 -0500

    Added a more problem

---
+++ CPP_Programming/lecture_08 (13-02-2026)/217_Contains_Duplicate.cpp
@@ -0,0 +1,34 @@
+// time complexity O(n2) nsquare
+
+// class Solution {
+// public:
+//     bool containsDuplicate(vector<int>& nums) {
+//         for(int i=0; i<nums.size(); i++){
+//             for(int j=i+1; j<nums.size(); j++){
+//                 if(nums[i]==nums[j]){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+    sort(nums.begin(), nums.end()); 
+
+    for(int i = 0; i < nums.size() - 1; i++) {
+        if(nums[i] == nums[i+1]) {
+            return true;  
+        }
+    }
+    return false;
+}
+};
